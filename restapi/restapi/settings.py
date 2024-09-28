e572fc7d8919ce14854aa32daa835eb6ed67b234
shovickbarua <shovickbarua92@gmail.com>
2024-09-28 12:58:51 +0600

    update

--- restapi/restapi/settings.py
+++ restapi/restapi/settings.py
@@ -99,7 +99,13 @@ DATABASES = {
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-    ]
+    ],
+     'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.UserRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'user': '10/hour',  
+    },
 }
 
 
